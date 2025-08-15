24d896e7d10986ac2c2ff89ff5414333954a4cb2
xsalles <xs.salles@gmail.com>
2025-08-14 21:15:46 -0300

    feat: created the third screen of onBoarding

--- app/screens/OnBoarding/data.ts
+++ app/screens/OnBoarding/data.ts
@@ -1,6 +1,7 @@
 import { OnBoardingProps } from "@/interfaces/template/OnBoarding";
 import AirPlane from "../../../assets/images/illustrations/airplane.svg"
 import Buildings from "../../../assets/images/illustrations/buildings.svg"
+import TravelBooking from "../../../assets/images/illustrations/travelBooking.svg"
 
 export const onBoardingData: OnBoardingProps[] = [
     {
@@ -21,10 +22,9 @@ export const onBoardingData: OnBoardingProps[] = [
         lineDirection: "center",
     },
     {
-        title: "Hóteis e voos de ótima qualidade!",
-        description: "Os melhores Hóteis e Voos com ótimos atendimentos.",
-        buttonText: "Próximo",
-        image: Buildings,
-        lineDirection: "center",
+        title: "O mundo é tão grande, Vá e se divirta!",
+        description: "Nós podemos te ajudar a descobrir os lugares mais bonitos do mundo!",
+        buttonText: "Começar",
+        image: TravelBooking,
     }
 ]
--- app/screens/OnBoarding/first.tsx
+++ app/screens/OnBoarding/first.tsx
@@ -4,6 +4,6 @@ import { onBoardingData } from "./data";
 
 export default function FirstOnBoarding() {
   return (
-      <OnBoardingPage {...onBoardingData[1]}/>
+      <OnBoardingPage {...onBoardingData[2]}/>
   );
 }
